3b5a7fe165214f693601410af8856e4112f8f7c0
Fredrik Skogman <skogman@gmail.com>
2016-09-30 22:06:43 +0200

    White space fixes.

--- io/pie_io_png.c
+++ io/pie_io_png.c
@@ -44,9 +44,9 @@ int png_f32_read(struct bitmap_f32rgb* bm, const char* path)
         }
 
         pngp = png_create_read_struct(PNG_LIBPNG_VER_STRING,
-                                         NULL,
-                                         NULL,
-                                         NULL);
+                                      NULL,
+                                      NULL,
+                                      NULL);
 
         if (pngp == NULL)
         {
