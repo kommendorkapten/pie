8062cd20ad6ce987fa884eaa272b07cb1604030e
Fredrik Skogman <skogman@gmail.com>
2016-10-22 11:54:01 +0200

    Updated html page.

--- assets/js/img.js
+++ assets/js/img.js
@@ -25,112 +25,92 @@ function get_appropriate_ws_url()
 }
 
 window.addEventListener("load", function(evt) {
-    var ws;
+    var wsCmd;
+    var wsHist;
+    var wsImg;
 
-    document.getElementById("open").onclick = function(evt) {
-        if (ws) {
-            return false;
+    wsCmd = new WebSocket(get_appropriate_ws_url(),"pie-cmd");
+    wsCmd.binaryType = "arraybuffer";
+    wsHist = new WebSocket(get_appropriate_ws_url(),"pie-hist");
+    wsHist.binaryType = "arraybuffer";
+    wsImg = new WebSocket(get_appropriate_ws_url(),"pie-img");
+    wsImg.binaryType = "arraybuffer";
+
+    wsCmd.onopen = function(evt) {
+        console.log("Opening websocket...");
+    }
+
+    wsCmd.onclose = function(evt) {
+        console.log("Closing websocket...");
+        ws = null;
+    }
+
+    wsCmd.onerror = function(evt) {
+        console.log("ERROR: " + evt.data);
+    }
+
+    wsCmd.onmessage = function(evt) {
+        console.log("ERROR: " + evt);
+    }
+
+    wsImg.onmessage = function(evt) {
+        var now = Date.now()
+        var dur = now - wsCmd.pieStartTs;
+
+        /* Keep start point to measure draw time */
+        wsCmd.pieStartTs = now;
+
+        console.log("RT in " + dur + "ms");
+        if (typeof evt.data === "string") {
+            conslog.log("ERROR: Got data: " + evt.data);
+        } else {
+            var pixels = new Uint8ClampedArray(evt.data);
+            var bm = new ImageData(pixels, 1024, 100);
+
+            /* Update canvas */
+            var c = document.getElementById("img_canvas");
+            var ctx = c.getContext("2d");
+            ctx.putImageData(bm, 0, 0);
+            now = Date.now();
+            dur = now - wsCmd.pieStartTs;
+            wsCmd.pieStartTs = now;
+            console.log("Draw image in " + dur + "ms");
         }
-        ws = new WebSocket(get_appropriate_ws_url(),"pie-cmd");
-        ws.binaryType = "arraybuffer";
-
-        ws.onopen = function(evt) {
-            console.log("Opening websocket...");
-        }
-
-        ws.onclose = function(evt) {
-            console.log("Closing websocket...");
-            ws = null;
-        }
-
-        ws.onmessage = function(evt) {
-            var now = Date.now()
-            var dur = now - ws.pieStartTs;
-            ws.pieStartTs = now;
-            /*console.log(evt);*/
-            console.log("RT in " + dur + "ms");
-            if (typeof evt.data === "string") {
-                conslog.log("Got data: " + evt.data);
-            } else {
-                var pixels = new Uint8ClampedArray(evt.data);
-                console.log("br: " + pixels.length);
-                var bm = new ImageData(pixels, 1024, 100);
-
-                console.log("r: " + bm.data[4*1023]);
-                console.log("g: " + bm.data[4*1023 + 1]);
-                console.log("b: " + bm.data[4*1023 + 2]);
-                console.log("a: " + bm.data[4*1023 + 3]);
-
-                /* Update canvas */
-                var c = document.getElementById("img_canvas");
-                var ctx = c.getContext("2d");
-                ctx.putImageData(bm, 0, 0);
-                now = Date.now();
-                dur = now - ws.pieStartTs;
-                ws.pieStartTs = now;
-                console.log("Draw image in " + dur + "ms");                
-            }
-        }
-
-        ws.onerror = function(evt) {
-            console.log("ERROR: " + evt.data);
-        }
-        return false;
-    };
+    }
 
     document.getElementById("get").onclick = function(evt) {
-        if (!ws) {
+        if (!wsCmd) {
             return false;
         }
 
-        ws.pieStartTs = Date.now();
-        ws.send("Give me data yo");
+        wsCmd.pieStartTs = Date.now();
+        wsCmd.send("LOAD");
 
         return false;
     };
 
-    document.getElementById("close").onclick = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        ws.close();
-        ws = null;
-        return false;
-    };
-/*
     document.getElementById("range1").oninput = function(evt) {
-        if (!ws) {
+        var targ;
+
+        if (!wsCmd) {
             return false;
         }
 
-        var targ;
         if (evt.target) {
             targ = evt.target;
         } else {
             tart = evt.srcElement;
         }
 
-        ws.send(targ.value);
+        if (targ.wsCall) {
+            clearTimeout(targ.wsCall);
+        }
+        targ.wsCall = setTimeout(function(){
+            wsCmd.pieStartTs = Date.now();
+            wsCmd.send("CONTR " + targ.value);
+        }, 100);
+
 
         return true;
     };
-*/
-    document.getElementById("range1").onchange = function(evt) {
-        if (!ws) {
-            return false;
-        }
-        console.log("on change done");
-        ws.pieStartTs = Date.now();
-        var targ;
-        if (evt.target) {
-            targ = evt.target;
-        } else {
-            tart = evt.srcElement;
-        }
-
-        ws.send(targ.value);
-
-        return true;
-    };
-
 });
