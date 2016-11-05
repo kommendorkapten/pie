44b7825c14c7348b0564d359335dd1f500383b54
Fredrik Skogman <skogman@gmail.com>
2016-11-05 21:01:06 +0100

    Added license.

--- wsrv/pie_session.c
+++ wsrv/pie_session.c
@@ -1,3 +1,16 @@
+/*
+* Copyright (C) 2016 Fredrik Skogman, skogman - at - gmail.com.
+* This file is part of pie project
+*
+* The contents of this file are subject to the terms of the Common
+* Development and Distribution License (the "License"). You may not use this
+* file except in compliance with the License. You can obtain a copy of the
+* License at http://opensource.org/licenses/CDDL-1.0. See the License for the
+* specific language governing permissions and limitations under the License. 
+* When distributing the software, include this License Header Notice in each
+* file and include the License file at http://opensource.org/licenses/CDDL-1.0.
+*/
+
 #include "pie_session.h"
 #include "../lib/hmap.h"
 #include <stdlib.h>
