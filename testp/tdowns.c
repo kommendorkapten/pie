df1c28ea33374647c892f929b6b2689eb78be55e
Fredrik Skogman <skogman@gmail.com>
2017-02-11 16:40:27 +0100

    Added parameters.

--- testp/tdowns.c
+++ testp/tdowns.c
@@ -1,27 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "../lib/timing.h"
 #include "../io/pie_io.h"
 #include "../pie_types.h"
 #include "../bm/pie_bm.h"
 #include "../bm/pie_dwn_smpl.h"
 
+#define MAX_PATH 256
+
 int main(int argc, char** argv)
 {
-        int ret;
         struct bitmap_f32rgb img;
         struct bitmap_f32rgb dwn;        
         struct bitmap_u8rgb out;
         struct timing t;
+        char fout[MAX_PATH] = {0};
+        char fin[MAX_PATH] = {0};
         suseconds_t dur;
-        
-        if (argc != 2)
+        int c;        
+        int max = 0;
+        int ret;
+
+        while ((c = getopt(argc, argv, "f:o:d:")) != -1)
         {
-                printf("Usage tdowns filename\n");
-                return -1;
+                switch (c)
+                {
+                case 'f':
+                        strncpy(fin, optarg, MAX_PATH);
+                        fin[MAX_PATH - 1] = 0;
+                        break;
+                case 'o':
+                        strncpy(fout, optarg, MAX_PATH);
+                        fin[MAX_PATH - 1] = 0;                        
+                        break;
+                case 'd':
+                        max = atoi(optarg);
+                        break;
+                }
+        }
+
+        if (strlen(fin) == 0)
+        {
+                printf("No input file provided\n");
+                return 1;
+        }
+
+        if (strlen(fout) == 0)
+        {
+                printf("No output file provided\n");
+                return 1;
+        }
+
+        if (max < 1)
+        {
+                printf("Invalid output dimension\n");
+                return 1;
         }
         
         timing_start(&t);
-        ret = pie_io_load(&img, argv[1]);
+        ret = pie_io_load(&img, fin);
         dur = timing_dur_usec(&t);
         if (ret)
         {
@@ -32,7 +71,7 @@ int main(int argc, char** argv)
         
         timing_start(&t);
 
-        if (pie_dwn_smpl(&dwn, &img, 960, 960))
+        if (pie_dwn_smpl(&dwn, &img, max, max))
         {
                 abort();
         }
@@ -43,7 +82,7 @@ int main(int argc, char** argv)
         bm_conv_bd(&out, PIE_COLOR_8B,
                    &dwn, PIE_COLOR_32B);
 
-        png_u8rgb_write("down.png", &out);
+        jpg_u8rgb_write(fout, &out, 95);
 
         bm_free_f32(&img);
         bm_free_f32(&dwn);        
