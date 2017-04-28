39ada5a6d1370fb7aadf2133c0f9001987b51061
Fredrik Skogman <skogman@gmail.com>
2017-04-28 20:34:44 +0200

    added more storage types.

--- dm/pie_storage.h
+++ dm/pie_storage.h
@@ -4,8 +4,10 @@
 #define __9332_PIE_STORAGE_H__
 #include <sqlite3.h>
 
-#define PIE_STG_ONLINE 1
+#define PIE_STG_ONLINE   1
 #define PIE_STG_NEARLINE 2
+#define PIE_STG_THUMB    3
+#define PIE_STG_PROXY    4
 
 struct pie_storage
 {
