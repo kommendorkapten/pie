4d674954bd59ce90bd68765a1f8f5b9e724e30b4
Fredrik Skogman <skogman@gmail.com>
2017-08-20 16:41:33 +0200

    Added timing information.

--- testp/imgread.c
+++ testp/imgread.c
@@ -2,10 +2,12 @@
 #include "../pie_types.h"
 #include "../bm/pie_bm.h"
 #include "../io/pie_io.h"
+#include "../lib/timing.h"
 
 int main(int argc, char** argv)
 {
         struct pie_bitmap_f32rgb bm;
+        struct timing t;
         int ret;        
 
         if (argc != 2)
@@ -13,6 +15,7 @@ int main(int argc, char** argv)
                 return -1;
         }
 
+        timing_start(&t);
 #if 0
         ret = png_f32_read(&bm, argv[1]);
 #else
@@ -23,6 +26,9 @@ int main(int argc, char** argv)
                 printf("Could not load media: %d\n", ret);
                 return ret;
         }
+        printf("Loaded %s in %0.3fs\n",
+               argv[1],
+               (float)timing_dur_msec(&t) / 1000.0f);
         printf("width:     %d\n", bm.width);
         printf("height:    %d\n", bm.height);
         printf("stride:    %d\n", bm.row_stride);
