d1f04e1935a618f2fa9b0781669e0ad4b1349601
Fredrik Skogman <skogman@gmail.com>
2017-09-03 14:26:04 +0200

    Added support for 16b output.

--- testp/tdowns.c
+++ testp/tdowns.c
@@ -9,12 +9,17 @@
 #include "../bm/pie_bm_dwn_smpl.h"
 
 #define MAX_PATH 256
+#define OUTPUT_16B 0
 
 int main(int argc, char** argv)
 {
         struct pie_bitmap_f32rgb img;
         struct pie_bitmap_f32rgb dwn;        
+#if OUTPUT_16B
+        struct pie_bitmap_u16rgb out;
+#else
         struct pie_bitmap_u8rgb out;
+#endif
         struct timing t;
         char fout[MAX_PATH] = {0};
         char fin[MAX_PATH] = {0};
@@ -79,13 +84,33 @@ int main(int argc, char** argv)
         dur = timing_dur_usec(&t);
         printf("Downsample took %luusec\n", dur);
 
-        pie_bm_conv_bd(&out, PIE_COLOR_8B,
-                       &dwn, PIE_COLOR_32B);
+        timing_start(&t);        
+        pie_bm_conv_bd(&out,
+#if OUTPUT_16B
+                       PIE_COLOR_16B,
+#else
+                       PIE_COLOR_8B,
+#endif
+                       &dwn,
+                       PIE_COLOR_32B);
+        dur = timing_dur_usec(&t);
+        printf("To 16bit took %luusec\n", dur);
 
-        jpg_u8rgb_write(fout, &out, 85);
+        timing_start(&t);        
+#if OUTPUT_16B
+        png_u16rgb_write(fout, &out);
+#else
+        png_u8rgb_write(fout, &out);
+#endif
+        dur = timing_dur_usec(&t);
+        printf("Write 16bit PNG took %luusec\n", dur);
 
         pie_bm_free_f32(&img);
         pie_bm_free_f32(&dwn);        
+#if OUTPUT_16B
+        pie_bm_free_u16(&out);
+#else
         pie_bm_free_u8(&out);
+#endif
         return 0;
 }
