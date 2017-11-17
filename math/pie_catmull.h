ff0f2547b8bedf008e175007975387e8d55af5d1
Fredrik Skogman <skogman@gmail.com>
2017-11-17 20:07:35 +0100

    updated comment

--- math/pie_catmull.h
+++ math/pie_catmull.h
@@ -6,7 +6,7 @@
 * Development and Distribution License (the "License"). You may not use this
 * file except in compliance with the License. You can obtain a copy of the
 * License at http://opensource.org/licenses/CDDL-1.0. See the License for the
-* specific language governing permissions and limitations under the License. 
+* specific language governing permissions and limitations under the License.
 * When distributing the software, include this License Header Notice in each
 * file and include the License file at http://opensource.org/licenses/CDDL-1.0.
 */
@@ -33,11 +33,10 @@ extern void pie_mth_catm_rom(struct pie_point_2d*,
  * Connecting points will be duplicated.
  * @param pointer to store generated curve. Buffer must be able to store
  *        number of splines * number of points elements.
- *        i.e for p0, p1, p2, p3, p4 and 25 points per segment, 2 * 25 
+ *        i.e for p0, p1, p2, p3, p4 and 25 points per segment, 2 * 25
  *        points will be generated (p0-p1,p1-p2).
- * @param pointer to four control points, including two control points.
- *        The first point is the first control point, and the last point
- *        is the last control point.
+ * @param pointer to control points, including two control points.
+ *        The first point is at index 1, and the last is num points - 2.
  * @param number of points (including control points).
  * @param number of points to draw per segment.
  * @return void
