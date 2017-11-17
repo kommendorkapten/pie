de64d2a7dbaa103a8f8f34ae4cfce83e9062dee4
Fredrik Skogman <skogman@gmail.com>
2017-11-17 20:10:14 +0100

    Fixed a bug that caused to small step size to be used.

--- math/pie_catmull.c
+++ math/pie_catmull.c
@@ -6,7 +6,7 @@
 * Development and Distribution License (the "License"). You may not use this
 * file except in compliance with the License. You can obtain a copy of the
 * License at http://opensource.org/licenses/CDDL-1.0. See the License for the
-* specific language governing permissions and limitations under the License. 
+* specific language governing permissions and limitations under the License.
 * When distributing the software, include this License Header Notice in each
 * file and include the License file at http://opensource.org/licenses/CDDL-1.0.
 */
@@ -26,7 +26,7 @@ void pie_mth_catm_rom(struct pie_point_2d* out,
         float t1 = pie_cm_tj(t0, &p[0], &p[1]);
         float t2 = pie_cm_tj(t1, &p[1], &p[2]);
         float t3 = pie_cm_tj(t2, &p[2], &p[3]);
-        float step = (t2 - t1) / (float)num_p;
+        float step = (t2 - t1) / (float)(num_p - 1);
         float t = t1;
 
         for (int i = 0; i < num_p; i++)
@@ -39,12 +39,12 @@ void pie_mth_catm_rom(struct pie_point_2d* out,
                 a2.y = ((t2 - t) * p[1].y)/(t2 - t1) + ((t - t1) * p[2].y)/(t2 - t1);
                 a3.x = ((t3 - t) * p[2].x)/(t3 - t2) + ((t - t2) * p[3].x)/(t3 - t2);
                 a3.y = ((t3 - t) * p[2].y)/(t3 - t2) + ((t - t2) * p[3].y)/(t3 - t2);
-                
+
                 b1.x = ((t2 - t) * a1.x)/(t2 - t0) + ((t - t0) * a2.x)/(t2 - t0);
                 b1.y = ((t2 - t) * a1.y)/(t2 - t0) + ((t - t0) * a2.y)/(t2 - t0);
                 b2.x = ((t3 - t) * a2.x)/(t3 - t1) + ((t - t1) * a3.x)/(t3 - t1);
                 b2.y = ((t3 - t) * a2.y)/(t3 - t1) + ((t - t1) * a3.y)/(t3 - t1);
-    
+
                 c.x = ((t2 - t) * b1.x)/(t2 - t1) + ((t - t1) * b2.x)/(t2 - t1);
                 c.y = ((t2 - t) * b1.y)/(t2 - t1) + ((t - t1) * b2.y)/(t2 - t1);
 
