02d8c487da4542eb0710e12a66e606918fb24b2b
Fredrik Skogman <skogman@gmail.com>
2018-08-10 10:14:04 +0200

    Only load harware EVP on SPARC/Solaris

--- testp/testfwlk.c
+++ testp/testfwlk.c
@@ -17,7 +17,7 @@ void cb_fun(const char*);
 int main(int argc, char** argv)
 {
 	char path[MAX_PATH];
-
+#if __sun && __sparc
         if (argc > 2 )
         {
                 printf("load engine\n");
@@ -25,12 +25,12 @@ int main(int argc, char** argv)
                 ENGINE_load_openssl();
                 ENGINE_load_dynamic();
                 ENGINE_load_cryptodev();
-                ENGINE_load_builtin_engines();        
+                ENGINE_load_builtin_engines();
                 OpenSSL_add_all_algorithms();
 
                 ENGINE* dyn;
                 int ok;
-        
+
                 dyn = ENGINE_by_id("dynamic");
                 if (dyn == NULL)
                 {
@@ -40,14 +40,16 @@ int main(int argc, char** argv)
                 if (!ok) printf("Failed to init\n");
                 ok = ENGINE_ctrl_cmd_string(dyn, "LOAD", NULL, 0);
                 if (!ok) printf("Failed to init\n");
-                
+
                 if (!ENGINE_init(dyn))
                 {
                         printf("Failed to init\n");
                 }
                 ENGINE_set_default(dyn, ENGINE_METHOD_ALL & ~ENGINE_METHOD_RAND);
 
-        }        
+        }
+#endif
+
 	if (argc > 1)
 	{
 		strncpy(path, argv[1], MAX_PATH);
@@ -58,7 +60,7 @@ int main(int argc, char** argv)
 	}
 
 	walk_dir(path, &cb_fun);
-	
+
 	return 0;
 }
 
@@ -74,7 +76,7 @@ void cb_fun(const char* p)
 	}
 	printf("%s %lld\n", p, stat_buf.st_size);
 #endif
-        
+
         char buf[BUF_LEN];
         int fd = open(p, O_RDONLY);
         ssize_t br;
@@ -86,24 +88,24 @@ void cb_fun(const char* p)
         const EVP_MD* md = EVP_sha1();
         ENGINE* impl = NULL;
 
-        
-        
+
+
         mdctx = EVP_MD_CTX_create();
         EVP_DigestInit_ex(mdctx, md, impl);
-        
+
         while ((br = read(fd, buf, BUF_LEN)) > 0)
         {
                 EVP_DigestUpdate(mdctx, buf, br);
                 size += br;
         }
-       
+
         close(fd);
 
         EVP_DigestFinal_ex(mdctx, sum, &md_len);
         EVP_MD_CTX_destroy(mdctx);
-        
+
         printf("%s %ld\n", p, size);
-        
+
         for (unsigned int i = 0; i < md_len; i++)
         {
                 printf("%02x", sum[i]);
