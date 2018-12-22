1ecd3376eaf5d82bd0c8b7f47e52eee07cb22764
Fredrik Skogman <skogman@gmail.com>
2018-12-22 09:39:49 +0100

    Updated test program.

--- testp/test_gamma.c
+++ testp/test_gamma.c
@@ -13,33 +13,32 @@
 
 int main(void)
 {
-        float ov[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
-        float v[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
-        float gcv[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
-        float ogcv[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
-        int len = sizeof(v) / sizeof(v[0]);
+        float gam[] =   {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
+        float lin[] =   {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
+        float gam_c[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
+        float lin_c[] = {0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.723f, 0.8f, 0.9f, 1.0f};
+        int len = sizeof(gam) / sizeof(gam[0]);
 
         /* To linear */
         for (int i = 0; i < len; i++)
         {
-                gcv[i] = pie_alg_srgb_to_linear(gcv[i]);
-                ogcv[i] = pie_alg_srgb_to_linear(ogcv[i]);
+                lin[i] = pie_alg_srgb_to_linear(lin[i]);
+                lin_c[i] = pie_alg_srgb_to_linear(lin_c[i]);
         }
 
-        pie_alg_contr(v, 1.1f, len, 1, 0);
-        pie_alg_contr(gcv, 1.1f, len, 1, 0);
-
-        /* To sRGB */
-        for (int i = 0; i < len; i++)
-        {
-                gcv[i] = pie_alg_linear_to_srgb(gcv[i]);
-        }
+        pie_alg_contr(gam_c, 1.1f, len, 1, 0);
+        pie_alg_contr(lin_c, 1.1f, len, 1, 0);
 
         /* Done */
-        printf("Org     contrast-g    lin     contrast-l\n");
+        printf("Gamma    linear   contrast-g contrast-l contrast-l-g\n");
         for (int i = 0; i < len; i++)
         {
-                printf("%f %f %f %f\n", ov[i], v[i], ogcv[i], gcv[i]);
+                printf("%f %f %f   %f %f\n",
+                       gam[i],
+                       lin[i],
+                       gam_c[i],
+                       lin_c[i],
+                       pie_alg_linear_to_srgb(lin_c[i]));
         }
 
         return 0;
