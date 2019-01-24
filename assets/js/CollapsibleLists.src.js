4a37bf466af5104d6079c228a102762e7a139e67
Fredrik Skogman <skogman@gmail.com>
2019-01-24 19:31:33 +0100

    White space fixes.

--- assets/js/CollapsibleLists.src.js
+++ assets/js/CollapsibleLists.src.js
@@ -13,103 +13,100 @@ http://creativecommons.org/publicdomain/zero/1.0/legalcode
 
 const CollapsibleLists = (function(){
 
-  // Makes all lists with the class 'collapsibleList' collapsible. The
-  // parameter is:
-  //
-  // doNotRecurse - true if sub-lists should not be made collapsible
-  function apply(doNotRecurse){
+    // Makes all lists with the class 'collapsibleList' collapsible. The
+    // parameter is:
+    //
+    // doNotRecurse - true if sub-lists should not be made collapsible
+    function apply(doNotRecurse){
 
-    [].forEach.call(document.getElementsByTagName('ul'), node => {
+        [].forEach.call(document.getElementsByTagName('ul'), node => {
 
-      if (node.classList.contains('collapsibleList')){
+            if (node.classList.contains('collapsibleList')){
 
-        applyTo(node, true);
+                applyTo(node, true);
 
-        if (!doNotRecurse){
+                if (!doNotRecurse){
 
-          [].forEach.call(node.getElementsByTagName('ul'), subnode => {
-            subnode.classList.add('collapsibleList')
-          });
+                    [].forEach.call(node.getElementsByTagName('ul'), subnode => {
+                        subnode.classList.add('collapsibleList')
+                    });
 
+                }
+
+            }
+
+        })
+
+    }
+
+    // Makes the specified list collapsible. The parameters are:
+    //
+    // node         - the list element
+    // doNotRecurse - true if sub-lists should not be made collapsible
+    function applyTo(node, doNotRecurse){
+        [].forEach.call(node.getElementsByTagName('li'), li => {
+
+            if (!doNotRecurse || node === li.parentNode){
+
+                li.style.userSelect       = 'none';
+                li.style.MozUserSelect    = 'none';
+                li.style.msUserSelect     = 'none';
+                li.style.WebkitUserSelect = 'none';
+
+                li.addEventListener('click', handleClick.bind(null, li));
+
+                toggle(li);
+
+            }
+
+        });
+
+    }
+
+    // Handles a click. The parameter is:
+    //
+    // node - the node for which clicks are being handled
+    function handleClick(node, e){
+        let li = e.target;
+        while (li.nodeName !== 'LI'){
+            li = li.parentNode;
         }
 
-      }
+        if (li === node){
+            toggle(node);
+        }
 
-    })
-
-  }
-
-  // Makes the specified list collapsible. The parameters are:
-  //
-  // node         - the list element
-  // doNotRecurse - true if sub-lists should not be made collapsible
-  function applyTo(node, doNotRecurse){
-
-    [].forEach.call(node.getElementsByTagName('li'), li => {
-
-      if (!doNotRecurse || node === li.parentNode){
-
-        li.style.userSelect       = 'none';
-        li.style.MozUserSelect    = 'none';
-        li.style.msUserSelect     = 'none';
-        li.style.WebkitUserSelect = 'none';
-
-        li.addEventListener('click', handleClick.bind(null, li));
-
-        toggle(li);
-
-      }
-
-    });
-
-  }
-
-  // Handles a click. The parameter is:
-  //
-  // node - the node for which clicks are being handled
-  function handleClick(node, e){
-
-    let li = e.target;
-    while (li.nodeName !== 'LI'){
-      li = li.parentNode;
     }
 
-    if (li === node){
-      toggle(node);
+    // Opens or closes the unordered list elements directly within the
+    // specified node. The parameter is:
+    //
+    // node - the node containing the unordered list elements
+    function toggle(node){
+        const open = node.classList.contains('collapsibleListClosed');
+        const uls  = node.getElementsByTagName('ul');
+
+        [].forEach.call(uls, ul => {
+
+            let li = ul;
+            while (li.nodeName !== 'LI'){
+                li = li.parentNode;
+            }
+
+            if (li === node){
+                ul.style.display = (open ? 'block' : 'none');
+            }
+
+        });
+
+        node.classList.remove('collapsibleListOpen');
+        node.classList.remove('collapsibleListClosed');
+
+        if (uls.length > 0){
+            node.classList.add('collapsibleList' + (open ? 'Open' : 'Closed'));
+        }
+
     }
-
-  }
-
-  // Opens or closes the unordered list elements directly within the
-  // specified node. The parameter is:
-  //
-  // node - the node containing the unordered list elements
-  function toggle(node){
-
-    const open = node.classList.contains('collapsibleListClosed');
-    const uls  = node.getElementsByTagName('ul');
-
-    [].forEach.call(uls, ul => {
-
-      let li = ul;
-      while (li.nodeName !== 'LI'){
-        li = li.parentNode;
-      }
-
-      if (li === node){
-        ul.style.display = (open ? 'block' : 'none');
-      }
-
-    });
-
-    node.classList.remove('collapsibleListOpen');
-    node.classList.remove('collapsibleListClosed');
-
-    if (uls.length > 0){
-      node.classList.add('collapsibleList' + (open ? 'Open' : 'Closed'));
-    }
-
-  }
 
     return {apply, applyTo, toggle};
 
